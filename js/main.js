57ce2d946fcc15773e0181ff2ff2a100bfa4978d
Alexander Rowland <arowland900@gmail.com>
2019-05-14 13:13:49 -0700

    refactor to SPA, step one

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
+// cached elements
 var h1 = document.querySelector('h1')
+var h2 = document.querySelector('h2')
+var img = document.querySelector('img')
+var p = document.querySelector('p')
+var contact = document.getElementById('contact')
+var about = document.getElementById('about')
+var loc = document.location.href
 
+var abtLink = document.getElementById('abtLink')
+var artLink = document.getElementById('artLink')
+var musLink = document.getElementById('musLink')
+var conLink = document.getElementById('conLink')
+
+// event listeners
 h1.addEventListener('click', function(){
-    document.location.href = './index.html';
+    loc = './index.html';
+    loadHome()
 })
+
+// functions
+function loadHome(){
+    h2.setAttribute('style', 'display: none');
+    p.setAttribute('style', 'display: none');
+    contact.setAttribute('style', 'display: none');
+}
