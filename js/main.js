9e321dc51da8bd0caaf3bc6094151ea500e13fba
Alexander Rowland <arowland900@gmail.com>
2019-05-14 13:49:13 -0700

    add soundcloud autoplay

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // cached elements
 var h1 = document.querySelector('h1')
-var h2 = document.querySelector('h2')
 var img = document.querySelector('img')
-var p = document.querySelector('p')
 var contact = document.getElementById('contact')
 var about = document.getElementById('about')
+var music = document.getElementById('music')
+
 var loc = document.location.href
 
 var abtLink = document.getElementById('abtLink')
@@ -25,22 +25,35 @@ conLink.addEventListener('click', function(){
     // loc = './index.html';
     loadContact()
 })
+musLink.addEventListener('click', function(){
+    // loc = './index.html';
+    loadMusic()
+})
 
 // functions
 function loadHome(){
     img.setAttribute('style', 'display: block');
     about.setAttribute('style', 'display: none');
     contact.setAttribute('style', 'display: none');
+    music.setAttribute('style', 'display: none');
 }
 function loadAbout(){
     img.setAttribute('style', 'display: none');
     about.setAttribute('style', 'display: block');
     contact.setAttribute('style', 'display: none');
+    music.setAttribute('style', 'display: none');
 }
 function loadContact(){
     img.setAttribute('style', 'display: none');
     about.setAttribute('style', 'display: none');
     contact.setAttribute('style', 'display: block');
+    music.setAttribute('style', 'display: none');
+}
+function loadMusic(){
+    img.setAttribute('style', 'display: none');
+    about.setAttribute('style', 'display: none');
+    contact.setAttribute('style', 'display: none');
+    music.setAttribute('style', 'display: block');
 }
 
 // on page load
